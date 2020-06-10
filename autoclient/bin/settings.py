936e429075a83556ea6959ce75c930bcab1af183
yasu <568200065@qq.com>
2020-06-10 22:49:43 +0900

    更新  优化代码

--- autoclient/bin/settings.py
+++ autoclient/bin/settings.py
@@ -1,18 +1,20 @@
+# 配置信息的实现
+
 import setting
 import global_settings
 
 
 class Settings():
     def __init__(self):
+        # 集成全局配置文件global_settings中的配置
+        for key in dir(global_settings):
+            if key.isupper():
+                setattr(self, key, getattr(global_settings, key))
+
         # 集成自定义配置文件setting中的配置
         for key in dir(setting):
             if key.isupper():
-                v = getattr(setting, key)
-                setattr(self, key, v)
+                setattr(self, key, getattr(setting, key))
 
-        for key in dir(global_settings):
-            if key.isupper():
-                v = getattr(global_settings, key)
-                setattr(self, key, v)
 
 settings = Settings()
--- autoclient/bin/start.py
+++ autoclient/bin/start.py
@@ -1,4 +1,8 @@
+# 启动
+
+
 from settings import settings
 
 if __name__ == '__main__':
     print(settings.USER)
+    print(settings.EMAIL_PORT)
