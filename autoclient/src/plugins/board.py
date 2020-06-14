3bad75dc7d119eb42da473f21c276b58e1709851
yasu <568200065@qq.com>
2020-06-14 23:51:01 +0900

    更新  完善采集的代码

--- autoclient/src/plugins/board.py
+++ autoclient/src/plugins/board.py
@@ -16,8 +16,8 @@ class Basic(object):
         if debug:
             res = open(os.path.join(settings.BASEDIR,'files/board.txt'), 'r', encoding='utf-8').read()
         else:
-            res = command_func('hostname')
-        self.parse(res)
+            res = command_func('sudo dmidecode -t1')
+        return self.parse(res)
 
     def parse(self, res):
         ### 数据筛选
@@ -34,5 +34,5 @@ class Basic(object):
                 v = info.strip().split(':')
                 if len(v) == 2:
                     if v[0] in key_map:
-                        result[key_map[v[0]]]=v[1]
+                        result[key_map[v[0]]]=v[1] 
         return result     
